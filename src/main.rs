0c1da602931d5c16ebb19c498c16921b720e2dc4
Izzy Whistlecroft <izzy.whistlecroft@nccgroup.com>
2019-01-28 13:44:26 +0000

    Change Easy instances to Easy2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write, stdout}; //, Result};
+use std::io::{BufRead, BufReader};
 extern crate curl;
-use curl::easy::Easy;
+use curl::easy::{Easy2, Handler, WriteError};
 //use std::io::{stdout, Write};
+
+struct Collector(Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0 = data.to_vec();
+        Ok(data.len())
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args);
-    let mut easy = Easy::new();
+    let mut easy = Easy2::new(Collector(Vec::new()));
+    easy.get(true).unwrap();
 
     let file = File::open(config.filename).unwrap();
     for line in BufReader::new(file).lines() {
@@ -36,21 +47,18 @@ impl Config {
     }
 }
 
-// This function takes an instance of "Easy", a base URL and a suffix
-// It then makes the request and will print the page it requested and the response
-// if the response was not a 404
-fn request(easy: &mut Easy, base: &str, end: &str) {
+// This function takes an instance of "Easy2", a base URL and a suffix
+// It then makes the request, if the response was not a 404
+// then it will print the URI it requested and the response
+fn request(easy: &mut Easy2<Collector>, base: &str, end: &str) {
     let url = format!("{}/{}", base, end);
 
     easy.url(&url).unwrap();
-    // let result_len = easy.write_function(|data| {
-    //     //stdout().write_all(data).unwrap();
-    //     //println!("{}", data.len());
-    //     //Ok(data.len())
-    //     Ok(data.len())
-    // }).unwrap();
     easy.perform().unwrap();
     let code = easy.response_code().unwrap();
-    let result_len = easy.size_download().unwrap();
-    if code != 404 { println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, result_len); }
+    let contents = easy.get_ref();
+
+
+    if code != 404 { println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, String::from_utf8_lossy(&contents.0).len()); }
+
 }
