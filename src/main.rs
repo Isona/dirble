25056d7b02babd3d99dafbdad9380cf1c603ed63
Izzy Whistlecroft <izzy.whistlecroft@nccgroup.com>
2019-01-29 16:01:46 +0000

    Add comments and line spacing

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,21 @@ impl Handler for Collector {
 
 
 fn main() {
+    // Load the yaml file containing argument definitions
     let yml = load_yaml!("args.yml");
     let m = App::from_yaml(yml).get_matches();
 
+    // Get the wordlist file from the arguments and open it
     let file = File::open(m.value_of("wordlist").unwrap()).unwrap();
 
+    // Get the host URI from the arguments
     let hostname = m.value_of("host").unwrap();
 
+    // Create a new curl Easy2 instance and set it to use GET requests
     let mut easy = Easy2::new(Collector(Vec::new()));
     easy.get(true).unwrap();
 
+    // For each line in the file, call the request function on it
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
         request(&mut easy, hostname, &line)
@@ -38,10 +43,14 @@ fn main() {
 // It then makes the request, if the response was not a 404
 // then it will print the URI it requested and the response
 fn request(easy: &mut Easy2<Collector>, base: &str, end: &str) {
+
+    //Concatenate and url encode the url, then set it in the Easy2 instance
     let url = format!("{}/{}", base, end);
     let url = utf8_percent_encode(&url, DEFAULT_ENCODE_SET).to_string();
-
     easy.url(&url).unwrap();
+
+    //Perform the request and check if it's empty
+    //If it's empty then output info and return
     match easy.perform() {
         Ok(_v) => {}
         Err(_e) => 
@@ -50,8 +59,16 @@ fn request(easy: &mut Easy2<Collector>, base: &str, end: &str) {
         }
     }
 
+    // Get the response code
     let code = easy.response_code().unwrap();
+    // Get the contents of the response
     let contents = easy.get_ref();
-    if code != 404 { println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, String::from_utf8_lossy(&contents.0).len()); }
+    
+    // Print some output if the 
+    if code != 404 
+    { 
+        let content_len = String::from_utf8_lossy(&contents.0).len();
+        println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, content_len); 
+    }
 
 }
