87aa1a01df785e3036d5072858a8d63b378e29c8
Izzy Whistlecroft <izzy.whistlecroft@nccgroup.com>
2019-02-01 10:35:39 +0000

    Split request function into new module

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,9 @@ use std::{
     time::Duration,
 };
 extern crate curl;
-use curl::easy::{Easy2, Handler, WriteError};
-use percent_encoding::{utf8_percent_encode, percent_decode, DEFAULT_ENCODE_SET};
+use curl::easy::{Easy2};
 mod arg_parse;
-
-struct Collector(Vec<u8>);
-
-impl Handler for Collector {
-    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
-        self.0 = data.to_vec();
-        Ok(data.len())
-    }
-}
-
+mod request;
 
 fn main() {
     // Load the yaml file containing argument definitions
@@ -93,76 +83,15 @@ fn main() {
     }
 }
 
-// This function takes an instance of "Easy2", a base URL and a suffix
-// It then makes the request, if the response was not a 404
-// then it will print the URI it requested and the response
-fn request(easy: &mut Easy2<Collector>, base: &str, end: &str) -> u32{
-
-    //Concatenate and url encode the url, then set it in the Easy2 instance
-    let url = format!("{}/{}", base, end);
-    let url = utf8_percent_encode(&url, DEFAULT_ENCODE_SET).to_string();
-    easy.url(&url).unwrap();
-
-    //Perform the request and check if it's empty
-    //If it's empty then output info and return
-    match easy.perform() {
-        Ok(_v) => {}
-        Err(_e) => {   println!("- {} (CODE: 0|SIZE: 0)", url);
-            return 0; 
-        }
-    }
-
-    // Get the response code
-    let code = easy.response_code().unwrap();
-    // Get the contents of the response
-    let contents = easy.get_ref();
-    
-    // Determine what to do based on the code
-    match code {
-        // If a 404 code, just return the response code
-        404 => return code,
-        // If it's a redirect, then check if the redirection destination is
-        // to the same URL with a / on the end, if so return 1 so that the
-        // calling thread knows this is a discovered folder
-        // Otherwise, just print some output and return the response code
-        301 | 302 => {
-            let content_len = String::from_utf8_lossy(&contents.0).len();
-
-            // Get and url decode the redirect destination
-            let redir_dest = easy.redirect_url().unwrap().unwrap();
-            let redir_dest = percent_decode(redir_dest.as_bytes()).decode_utf8().unwrap();
-
-            // Clone and url decode the url
-            let dir_url = url.clone() + "/";
-            let dir_url = percent_decode(dir_url.as_bytes()).decode_utf8().unwrap();
-
-            if dir_url == redir_dest {
-                println!("==> DIRECTORY: {}", url);
-                1
-            }
-            else {
-                println!("+ {} (CODE: {}|SIZE:{:#?}|DEST:{})", url, code, content_len, redir_dest);
-                code
-            }
-        },
-        // If it's anything else, print out some information and return the response code
-        _ => {
-            let content_len = String::from_utf8_lossy(&contents.0).len();
-            println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, content_len); 
-            code
-        },
-    }
-}
-
 fn thread_spawn(tx: mpsc::Sender<String>, hostname: String, wordlist: Arc<Vec<String>>) {
     println!("Scanning {}/", hostname);
     // Create a new curl Easy2 instance and set it to use GET requests
-    let mut easy = Easy2::new(Collector(Vec::new()));
+    let mut easy = Easy2::new(request::Collector(Vec::new()));
     easy.get(true).unwrap();
 
     // For each item in the wordlist, call the request function on it
     for line in 0..wordlist.len() {
-        let code = request(&mut easy, &hostname, &wordlist[line]);
+        let code = request::make_request(&mut easy, &hostname, &wordlist[line]);
         match code {
             1 => {
                 let dir_url = format!("{}/{}", &hostname, &wordlist[line]);
---
+++ src/request.rs
@@ -0,0 +1,73 @@
+use percent_encoding::{utf8_percent_encode, percent_decode, DEFAULT_ENCODE_SET};
+extern crate curl;
+use curl::easy::{Easy2, Handler, WriteError};
+
+pub struct Collector(pub Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0 = data.to_vec();
+        Ok(data.len())
+    }
+}
+
+// This function takes an instance of "Easy2", a base URL and a suffix
+// It then makes the request, if the response was not a 404
+// then it will print the URI it requested and the response
+pub fn make_request(easy: &mut Easy2<Collector>, base: &str, end: &str) -> u32{
+
+    //Concatenate and url encode the url, then set it in the Easy2 instance
+    let url = format!("{}/{}", base, end);
+    let url = utf8_percent_encode(&url, DEFAULT_ENCODE_SET).to_string();
+    easy.url(&url).unwrap();
+
+    //Perform the request and check if it's empty
+    //If it's empty then output info and return
+    match easy.perform() {
+        Ok(_v) => {}
+        Err(_e) => {   println!("- {} (CODE: 0|SIZE: 0)", url);
+            return 0; 
+        }
+    }
+
+    // Get the response code
+    let code = easy.response_code().unwrap();
+    // Get the contents of the response
+    let contents = easy.get_ref();
+    
+    // Determine what to do based on the code
+    match code {
+        // If a 404 code, just return the response code
+        404 => return code,
+        // If it's a redirect, then check if the redirection destination is
+        // to the same URL with a / on the end, if so return 1 so that the
+        // calling thread knows this is a discovered folder
+        // Otherwise, just print some output and return the response code
+        301 | 302 => {
+            let content_len = String::from_utf8_lossy(&contents.0).len();
+
+            // Get and url decode the redirect destination
+            let redir_dest = easy.redirect_url().unwrap().unwrap();
+            let redir_dest = percent_decode(redir_dest.as_bytes()).decode_utf8().unwrap();
+
+            // Clone and url decode the url
+            let dir_url = url.clone() + "/";
+            let dir_url = percent_decode(dir_url.as_bytes()).decode_utf8().unwrap();
+
+            if dir_url == redir_dest {
+                println!("==> DIRECTORY: {}", url);
+                1
+            }
+            else {
+                println!("+ {} (CODE: {}|SIZE:{:#?}|DEST:{})", url, code, content_len, redir_dest);
+                code
+            }
+        },
+        // If it's anything else, print out some information and return the response code
+        _ => {
+            let content_len = String::from_utf8_lossy(&contents.0).len();
+            println!("+ {} (CODE:{}|SIZE:{:#?})", url, code, content_len); 
+            code
+        },
+    }
+}
