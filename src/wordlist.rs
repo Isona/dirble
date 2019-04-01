37dcd2ea0cd3f7951f350e9c295ed224308d09f7
Izzy Whistlecroft <izzy.whistlecroft@nccgroup.com>
2019-04-01 10:20:06 +0100

    Add license including preamble to all source files

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -1,3 +1,20 @@
+// This file is part of Dirble - https://www.github.com/nccgroup/dirble
+// Copyright (C) 2019 Izzy Whistlecroft <Izzy(dot)Whistlecroft(at)nccgroup(dot)com>
+// Released as open source by NCC Group Plc - https://www.nccgroup.com/
+//
+// Dirble is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Dirble is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Dirble.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::process::exit;
 use std::{
     sync::Arc,
