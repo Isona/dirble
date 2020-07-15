215e5f99075382aea422d5c1117452bbc692e3fe
David Young <david.young@nccgroup.com>
2020-07-15 17:15:55 +0100

    Add dynamic test against Python servers
    One flask server that covers a variety of response codes, one asyncio server that sends a chunked response.

---
+++ test/flask-server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from flask import Flask, url_for, redirect, abort, Response
+
+# This needs to be global so that the decorators work
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "Index page"
+
+@app.route("/302.html")
+def code302():
+	return redirect(url_for("index"), 302)
+
+@app.route("/401.html")
+def code401():
+	abort(401)
+
+@app.route("/403.html")
+def code403():
+	abort(403)
+
+@app.route("/429.html")
+def code429():
+	abort(429)
+
+@app.route("/chunked.html")
+def chunked():
+	def generate():
+		s = "This is a very long string. " * 50000
+		for i in range(10):
+			yield "This is line number {} with a long string {}\n".format(i, s)
+	return Response(generate(), mimetype="text/plain")
+
+def main():
+	app.run(debug=True, host="::1", port=5000, threaded=True)
+
+if __name__ == "__main__":
+	main()
